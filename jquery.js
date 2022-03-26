e80696f4a528e3b4c4e9f14cf3f62364ac5bee6b
Leatifuller <97773175+Leatifuller@users.noreply.github.com>
2022-03-25 19:04:42 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-(document).ready(function () {
+$(document).ready(function () {
     let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
     let sentencesIndex = 0;
     let letterIndex = 0;
